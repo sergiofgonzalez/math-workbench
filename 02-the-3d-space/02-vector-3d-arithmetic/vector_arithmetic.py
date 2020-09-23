9549a48d36caf31b76519239ed491e0a91f6b051
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2020-09-23 19:16:35 +0200

    Add more 3D content

--- 02-the-3d-space/02-vector-3d-arithmetic/vector_arithmetic.py
+++ 02-the-3d-space/02-vector-3d-arithmetic/vector_arithmetic.py
@@ -8,3 +8,19 @@ def add(*vectors):
   by_coordinate_list = zip(*vectors)
   sum_by_coordinate_list = [sum(coords) for coords in by_coordinate_list]
   return tuple(sum_by_coordinate_list)
+
+def subtract(v, w):
+  """Return the vector resulting from subtracting w from v
+
+  The vectors must be given in their Cartesian coordinates.
+  """
+  return tuple( v[i] - w[i] for i in range(0, len(v)) )
+
+def scalar_product(factor, v):
+  """
+  Return the vector resulting from multiplying the scalar factor by the vector v
+
+  Factor is any real number, and v is a vector given in their Cartesian coordinates.
+  """
+  return tuple( factor * v_i for v_i in v )
+
