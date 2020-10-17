0b0fcd4ccdfb5e7adba7d76c9ae9dfb22dbffa75
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2020-10-17 13:59:32 +0200

    Add matrices arithmetics

---
+++ 04-using-matrices-with-transformations/e00-my-libs/README.md
@@ -0,0 +1,20 @@
+# My libs
+> the collection of libraries and support functions used throughout the book but written by me
+
+
+## Description
+
++ `my_vectors.py` &mdash; library of vector related functions to add vectors, cross product, etc., along with some support functions that transform degrees to radians, and vice versa, etc.
++ `my_transformations.py` &mdash; library of transformations such as `scale_by(...)`, `translate_by(...)`, `rotate_*_by(...)`. Also, `linear_combination(...)` is defined here.
++ `my_matrices.py` &mdash; librate of matrix related functions to multiply matrices and vectors, multiply matrices, etc.
+
+
+Remember that you run the project you must do:
+```bash
+
+$ pwd
+[...]/03-transformations/e03-my-implementation
+
+# it must be run within the project directory so that teapot.off is found
+$ python3 testing_my_impls.py
+```
---
+++ 04-using-matrices-with-transformations/e00-my-libs/my_transformations.py
@@ -0,0 +1,71 @@
+from my_vectors import add, scale, to_polar, to_cartesian
+
+# Pre-existing
+def polygon_map(transformation, polygons):
+    return [
+        [transformation(vertex) for vertex in polygon]
+        for polygon in polygons
+    ]
+
+# added on exercise 4.1
+def translate_by(vector):
+    def new_function(v):
+        return add(vector, v)
+    return new_function
+
+# added on exercise 4.3
+def scale_by(factor):
+    def new_function(v):
+        return scale(factor, v)
+    return new_function
+
+# added on exercise 4.4
+# superseded by compose(*fArgs) on exercise 4.6
+# def compose(f_last, f_first):
+#     def new_function(input):
+#         return f_last(f_first(input))
+#     return new_function
+
+
+# added on exercise 4.6
+def compose(*fArgs):
+    def new_function(input):
+        state = input
+        for f in reversed(fArgs):
+            state = f(state)
+        return state
+    return new_function
+
+# added on exercise 4.7
+def rotate_x_by(angle):
+    def new_function(v):
+        x, y, z = v
+        vector_length, vector_angle = to_polar((y, z))
+        new_vector_angle = vector_angle + angle
+        new_y, new_z = to_cartesian((vector_length, new_vector_angle))
+        return (x, new_y, new_z)
+    return new_function
+
+
+def rotate_y_by(angle):
+    def new_function(v):
+        x, y, z = v
+        vector_length, vector_angle = to_polar((x, z))
+        new_vector_angle = vector_angle + angle
+        new_x, new_z = to_cartesian((vector_length, new_vector_angle))
+        return (new_x, y, new_z)
+    return new_function
+
+def rotate_z_by(angle):
+    def new_function(v):
+        x, y, z = v
+        vector_length, vector_angle = to_polar((x, y))
+        new_vector_angle = vector_angle + angle
+        new_x, new_y = to_cartesian((vector_length, new_vector_angle))
+        return (new_x, new_y, z)
+    return new_function
+
+# added on chapter 4
+def linear_combination(scalars, *vectors):
+    scaled = [scale(s, v) for s,v in zip(scalars, vectors)]
+    return add(*scaled)
