69bc359042186888fb5179824483b0c993c18267
Sergio F. Gonzalez <sergio.f.gonzalez@accenture.com>
2020-11-05 20:08:14 +0100

    Add vector in higher dims concepts

---
+++ 05-generalizations-for-higher-dimensions/e01-vector-lib/vector2d.py
@@ -0,0 +1,18 @@
+from vector import Vector
+
+class Vec2(Vector):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def scale(self, scalar):
+        return Vec2(scalar * self.x, scalar * self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return 'Vec2({}, {})'.format(self.x, self.y)  
